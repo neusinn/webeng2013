e399a4a61d5fc08dff640f34a42ea4f497529c8f
Markus von Allmen <neusinngmail.com>
2013-12-11 00:50:54 +0100

    bootstrap version

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>Willkommen!</title>
+</head>
+<body>
+
+<?php 
+  // Show all information
+  phpinfo()
+?>
+
+</body>
+</html>
